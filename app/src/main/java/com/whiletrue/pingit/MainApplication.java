99e1802b1730edf916961dc6c765d8b53d69ece4
Stuart Bowman <stuartsoft2012@gmail.com>
2016-01-23 16:23:51 -0500

    Added Fabric and Crashlytics

--- app/src/main/java/com/whiletrue/pingit/MainApplication.java
+++ app/src/main/java/com/whiletrue/pingit/MainApplication.java
@@ -3,6 +3,8 @@ package com.whiletrue.pingit;
 import android.app.Application;
 
 import com.squareup.leakcanary.LeakCanary;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by BOWMANRS1 on 1/23/2016.
@@ -11,7 +13,9 @@ public class MainApplication extends Application{
 
     @Override public void onCreate(){
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         LeakCanary.install(this);
+        throw new RuntimeException("This is a crash");
     }
 
 }
