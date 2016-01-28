b36c431e4a2e36bed6e8b5c37b610c09a18d343b
Stuart Bowman <stuartsoft2012@gmail.com>
2016-01-28 00:32:24 -0500

    Got the basic 3 tab layout running using the action bar tab template provided by Android Studio. Also setup a basic login button at startup

--- app/src/main/java/edu/gcc/whiletrue/pingit/MainActivity.java
+++ app/src/main/java/edu/gcc/whiletrue/pingit/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.gcc.whiletrue.pingit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -18,14 +19,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
