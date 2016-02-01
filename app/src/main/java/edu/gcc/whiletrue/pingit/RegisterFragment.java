9a1c29253ad77aaae6b95a0c6d9afa7cecc02572
Stuart Bowman <stuartsoft2012@gmail.com>
2016-02-01 00:56:48 -0500

    The 'Already have an account' button will now switch the fragment to the login fragment

--- app/src/main/java/edu/gcc/whiletrue/pingit/RegisterFragment.java
+++ app/src/main/java/edu/gcc/whiletrue/pingit/RegisterFragment.java
@@ -1,9 +1,13 @@
 package edu.gcc.whiletrue.pingit;
 
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +16,29 @@ import android.widget.Button;
 
 public class RegisterFragment extends Fragment {
 
+    OnHeadlineSelectedListener mCallback;
+
+    // Container Activity must implement this interface
+    public interface OnHeadlineSelectedListener {
+        public void onSwitchToLogin();
+    }
+
     public RegisterFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // This makes sure that the container activity has implemented
+        // the callback interface. If not, it throws an exception
+        try {
+            mCallback = (OnHeadlineSelectedListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(getActivity().toString()
+                    + " must implement OnHeadlineSelectedListener");
+        }
     }
 
     @Override
@@ -33,6 +58,14 @@ public class RegisterFragment extends Fragment {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), HomeActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        Button btnSwitchToLogin = (Button) view.findViewById(R.id.switchToLoginBtn);
+        btnSwitchToLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                mCallback.onSwitchToLogin();
             }
         });
 
--- app/src/main/java/edu/gcc/whiletrue/pingit/StartupActivity.java
+++ app/src/main/java/edu/gcc/whiletrue/pingit/StartupActivity.java
@@ -8,7 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class StartupActivity extends AppCompatActivity {
+public class StartupActivity extends AppCompatActivity implements RegisterFragment.OnHeadlineSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +28,15 @@ public class StartupActivity extends AppCompatActivity {
 
     }
 
+
+    //This will be called by the foreground fragment when the user wants to switch to the Login view
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onSwitchToLogin() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        LoginFragment loginfrag = new LoginFragment();
+        fragmentTransaction.replace(R.id.startup_fragment_container,loginfrag);
+        fragmentTransaction.commit();
     }
 }
