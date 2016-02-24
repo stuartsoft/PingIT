835ce70c4e2cb6b2f964ff4497bdba40540d9d66
Stuart Bowman <stuartsoft2012@gmail.com>
2016-02-23 20:12:10 -0500

    Added test 46

--- app/src/androidTest/java/edu/gcc/whiletrue/pingit/FAQUITest.java
+++ app/src/androidTest/java/edu/gcc/whiletrue/pingit/FAQUITest.java
@@ -13,6 +13,7 @@ import static android.support.test.espresso.Espresso.openActionBarOverflowOrOpti
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
@@ -41,6 +42,25 @@ public class FAQUITest {
 
         //assert that we are on the Settings page
         onView(withId(R.id.settingsFragmentContainer)).check(matches(isDisplayed()));
+    }
+
+    //Check that pressing the up button on the settings page brings us back to the previous tab on the home screen
+    @Test
+    public void test46(){
+        //tap FAQ tab
+        onView(withText(R.string.faqSectionTitle)).perform(click());
+        //assert that we are on the FAQ tab
+        onView(withId(R.id.fragment_faqpage)).check(matches(isDisplayed()));
+        //tap the Settings icon
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Settings"))
+                .perform(click());
+        //assert that we are on the Settings page
+        onView(withId(R.id.settingsFragmentContainer)).check(matches(isDisplayed()));
+
+        onView(withContentDescription("Navigate up")).perform(click());
+        //assert that we are on the FAQ tab
+        onView(withId(R.id.fragment_faqpage)).check(matches(isDisplayed()));
     }
 
     //go from faq page to pings page
