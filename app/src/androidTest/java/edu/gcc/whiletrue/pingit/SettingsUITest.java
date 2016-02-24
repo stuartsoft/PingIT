f2380fa2ff2dfb2e61f92e0d4be56a2e1758a1a2
Stuart Bowman <stuartsoft2012@gmail.com>
2016-02-23 23:43:30 -0500

    Added test 46

--- app/src/androidTest/java/edu/gcc/whiletrue/pingit/SettingsUITest.java
+++ app/src/androidTest/java/edu/gcc/whiletrue/pingit/SettingsUITest.java
@@ -8,10 +8,16 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
 import static android.support.test.espresso.matcher.PreferenceMatchers.withKey;
+import static android.support.test.espresso.matcher.PreferenceMatchers.withTitleText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withTagKey;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
@@ -26,6 +32,16 @@ public class SettingsUITest {
     public ActivityTestRule<SettingsActivity> mActivityRule = new ActivityTestRule<>(
             SettingsActivity.class);
 
+    //just test that the dialog box appears to change the display name. Then cancel the dialog
+    @Test
+    public void test46(){
+        onData(withKey("display_name")).perform(click());
+        //type a name
+        //onView(withText("Display Name")).perform(typeTextIntoFocusedView("Stuart Bowman"), closeSoftKeyboard());
+
+        //wait for dialog to appear, then dismiss it
+        pressBack();
+    }
 
     //tap logout button and make sure confirmation dialog appears
     @Test
