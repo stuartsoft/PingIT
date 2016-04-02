30e2d44ab55c4c266f3d435b3d855c01c99ce31c
Ben Stegner <stegnersaurus@gmail.com>
2016-04-02 14:05:19 -0400

    Overrided parse notification receiver, trying to make it play a custom notification sound

--- app/src/main/java/edu/gcc/whiletrue/pingit/MyParsePushBroadcastReceiver.java
+++ app/src/main/java/edu/gcc/whiletrue/pingit/MyParsePushBroadcastReceiver.java
@@ -1,6 +1,7 @@
 package edu.gcc.whiletrue.pingit;
 
 import android.app.Notification;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -16,10 +17,15 @@ public class MyParsePushBroadcastReceiver extends ParsePushBroadcastReceiver {
     @Override
     protected Notification getNotification(Context context, Intent intent) {
         Log.d("Testing", "Fired notification class");
+        Log.d("Testing", "Intent is " + intent);
 
         Notification n = super.getNotification(context, intent);
-        n.sound = Uri.parse("android.resource://" + context.getPackageName() + "/" + "R.raw.testsound.mp3");
+        n.sound = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.testsound);
+        //n.sound = Uri.parse("content://media/internal/audio/media/21");
+
         Log.d("Testing", "N equals " + n);
+        Log.d("Testing", "N sound equals " + n.sound);
+
         return n;
     }
 }
