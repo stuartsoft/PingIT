c35b35adcf2699a2422d25659a9ef9b90253f275
ZARD <zaredsoutdoors@gmail.com>
2016-04-03 15:28:26 -0400

    Changed tab underline color to white.

--- app/src/main/java/edu/gcc/whiletrue/pingit/HomeActivity.java
+++ app/src/main/java/edu/gcc/whiletrue/pingit/HomeActivity.java
@@ -1,11 +1,13 @@
 package edu.gcc.whiletrue.pingit;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
