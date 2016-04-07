6af54e950dbaa2e0d54ce393dbe0866f213505cf
Stuart Bowman <stuartsoft2012@gmail.com>
2016-04-07 13:04:45 -0400

    More revisions to the settingsActivity onDestroy and updated tests in SettingsUITest to login and logout correctly

--- app/src/main/java/edu/gcc/whiletrue/pingit/SettingsActivity.java
+++ app/src/main/java/edu/gcc/whiletrue/pingit/SettingsActivity.java
@@ -44,11 +44,19 @@ public class SettingsActivity extends AppCompatActivity {
 
         if (switchStateBool) {//If the user chooses to resend, get their chosen value and send to Parse
             String listStateString = preferences.getString(listPrefKey, "");
-
-            StringTokenizer tokenizer = new StringTokenizer(listStateString);
-            String resendValueString = tokenizer.nextToken();
-            String resendMinutesOrHours = tokenizer.nextToken();
-            int resendValueInt = Integer.parseInt(resendValueString);
+            int resendValueInt = 0;
+            String resendMinutesOrHours = "minutes";
+            try {
+                StringTokenizer tokenizer = new StringTokenizer(listStateString);
+                String resendValueString = tokenizer.nextToken();
+                resendMinutesOrHours = tokenizer.nextToken();
+                resendValueInt = Integer.parseInt(resendValueString);
+            }catch(Exception e){
+                Log.e(getString(R.string.log_error), getString(R.string.errorParsingSharedPrefs));
+                //allow it to continue and store the default values
+                resendValueInt = 0;
+                resendMinutesOrHours = "minutes";
+            }
 
             if (resendMinutesOrHours.contains("hour")) resendValueInt *= 60;//Convert hours to mins
 
