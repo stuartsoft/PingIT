1cdbf17c083149f36af859b3318a4a7d1f1517de
Stuart Bowman <stuartsoft2012@gmail.com>
2016-04-19 22:45:41 -0400

    Enabled Crashlytics/Fabric for Google Play deployment

--- app/src/main/java/edu/gcc/whiletrue/pingit/MainApplication.java
+++ app/src/main/java/edu/gcc/whiletrue/pingit/MainApplication.java
@@ -2,8 +2,11 @@ package edu.gcc.whiletrue.pingit;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.parse.Parse;
 import com.parse.ParseUser;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MainApplication extends Application{
 
@@ -12,11 +15,11 @@ public class MainApplication extends Application{
 
     @Override public void onCreate(){
         super.onCreate();
-        //Fabric.with(this, new Crashlytics());
         //LeakCanary.install(this);
-
         Parse.enableLocalDatastore(this);
         Parse.initialize(this);
+
+        Fabric.with(this, new Crashlytics());
     }
 
     @Override
